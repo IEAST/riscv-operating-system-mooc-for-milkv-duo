9221161d86a1bdaf95d840f27c75304d6f040b42
EastDong <xudong23@iscas.ac.cn>
2023-11-14 14:52:31 +0800

    04 multitask

---
+++ code/04-multitask/kernel.c
@@ -0,0 +1,25 @@
+#include "include/os.h"
+
+extern void dw8250_uart_init(void);
+extern void page_init(void);
+extern void sched_init(void);
+extern void schedule(void);
+extern void os_main(void);
+
+void start_kernel(void)
+{
+    dw8250_uart_init();
+    dw8250_uart_puts("Hello, RVOS!\n");
+
+    page_init();
+
+    sched_init();
+
+    os_main();
+
+    schedule();
+
+    dw8250_uart_puts("Would not go here!\n");
+    // while(1){};
+
+}
