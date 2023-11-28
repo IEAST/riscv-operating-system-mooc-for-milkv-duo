c534a3c61bd3cbd2014eba1c68b1fa13b78a6247
EastDong <xudong23@iscas.ac.cn>
2023-11-28 17:16:03 +0800

    Add duo qemu to support make run.

--- code/common.mk
+++ code/common.mk
@@ -1,8 +1,11 @@
 CROSS_COMPILE = riscv64-elf-
-CFLAGS = -nostdlib -fno-builtin -mcmodel=medany -g -Wall -Iinclude
+CFLAGS = -nostdlib -fno-builtin -mcmodel=medany -march=rv64imad -g -Wall -Iinclude
 
 GDB = riscv64-elf-gdb
 CC = ${CROSS_COMPILE}gcc
 OBJCOPY = ${CROSS_COMPILE}objcopy
 OBJDUMP = ${CROSS_COMPILE}objdump
 LD = ${CROSS_COMPILE}ld
+
+QEMU = ~/Tmp/plct-qemu/build/qemu-system-riscv64 
+QFLAGS = -M milkv_duo -nographic -smp 1 -bios none
