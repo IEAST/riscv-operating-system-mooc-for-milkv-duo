5bb266c199d614108c62d251f86fe48a8faa6fb5
EastDong <xudong23@iscas.ac.cn>
2024-01-30 10:01:52 +0800

    Support for ubuntu systems

--- code/common.mk
+++ code/common.mk
@@ -1,11 +1,11 @@
-CROSS_COMPILE = riscv64-elf-
-CFLAGS = -nostdlib -fno-builtin -mcmodel=medany -march=rv64imad -g -Wall -Iinclude
+CROSS_COMPILE =  riscv64-unknown-elf-
+CFLAGS = -nostdlib -fno-builtin -mcmodel=medany -march=rv64imafd -g -Wall -Iinclude
 
-GDB = riscv64-elf-gdb
+#GDB = riscv64-elf-gdb
 CC = ${CROSS_COMPILE}gcc
 OBJCOPY = ${CROSS_COMPILE}objcopy
 OBJDUMP = ${CROSS_COMPILE}objdump
 LD = ${CROSS_COMPILE}ld
 
-QEMU = ~/Tmp/plct-qemu/build/qemu-system-riscv64 
+#QEMU = ~/Tmp/plct-qemu/build/qemu-system-riscv64 
 QFLAGS = -M milkv_duo -nographic -smp 1 -bios none
